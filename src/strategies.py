f190e3f9a35f9a246cc808c15257dcb58eb3b7ac
Dylan Zenner <dzenner@phantomdata.io>
2021-12-06 19:50:28 -0600

    Update strategies.py

--- src/strategies.py
+++ src/strategies.py
@@ -1,18 +1,52 @@
 import numpy as np
 import talib
+import alpaca_trade_api as tradeapi
+from src.main import *
+
 
 closing_prices = []
+in_position = False
 
 
 class Strategy:
     def __init__(self):
-        pass
+        self.api = tradeapi.REST(
+            key_id=bot.api_key,
+            secret_key=bot.api_secret,
+            base_url=bot.base_url,
+            api_version="v2",
+        )
+
+    def order(self, side, qty, type, time_in_force, limit_price=None):
+        """
+        The order method to implment the buy/sell functionality of Afk Trader
+        :param side: buy or sell
+        :param qty: amount of the stock you would like to buy
+        :param type: type of trade to be made usually 'market'
+        :param time_in_force: how long an order will remain active before it is executed or expired
+        :param limit_price: limit price of the side if available
+        :return: True or False
+        """
+        try:
+            print("Sending order")
+            order_val = self.api.submit_order(
+                side=side,
+                qty=qty,
+                symbol=bot.symbol,
+                type=type,
+                time_in_force=time_in_force,
+                limit_price=limit_price,
+            )
+            print(order_val)
+            return True
+        except Exception as e:
+            print("An exception occured - {}".format(e))
+            return False
 
     def rsi_indicator(self, RSI_PERIOD, RSI_OVERBOUGHT, RSI_OVERSOLD, message):
         """
         Basic trading stretegy implemented using a RSI value to determine if the stock is overbought or oversold. And
         sends buy/sell orders accordingly to the Alpaca API
-
         :param RSI_PERIOD: The period used to calculate the RSI value
         :param RSI_OVERBOUGHT: The RSI value indicating when a stock should be considered overbought
         :param RSI_OVERSOLD: The RSI value indicating when a stock should be considered oversold
@@ -20,11 +54,12 @@ class Strategy:
         :return: None
         """
 
+        global in_position
+
         self.message = message
         self.RSI_PERIOD = RSI_PERIOD
         self.RSI_OVERBOUGHT = RSI_OVERBOUGHT
         self.RSI_OVERSOLD = RSI_OVERSOLD
-        self.in_position = False
 
         print(self.message)
         open_price = self.message[0]["o"]
@@ -48,22 +83,22 @@ class Strategy:
             print("The current rsi value is {}".format(last_rsi_value))
 
             if last_rsi_value > self.RSI_OVERBOUGHT:
-                if self.in_position:
+                if in_position:
                     print("The Stock Is Overbought: SELL NOW!!!")
                     # put alpaca sell logic here
-                    order_succeeded = self.api.order(
+                    order_succeeded = self.order(
                         side="sell", qty=1, type="market", time_in_force="day"
                     )
 
                     if order_succeeded:
-                        self.in_position = False
+                        in_position = False
                 else:
                     print(
                         "The stock is overbought but, you do not own any. So, you are unable to sell."
                     )
 
             if last_rsi_value < self.RSI_OVERSOLD:
-                if self.in_position:
+                if in_position:
                     print(
                         "The Stock Is oversold but, you already own it. So, you are unable to buy."
                     )
@@ -74,4 +109,4 @@ class Strategy:
                         side="buy", qty=1, type="market", time_in_force="day"
                     )
                     if order_succeeded:
-                        self.in_position = True
+                        in_position = True
